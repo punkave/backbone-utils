6939461ad06d169b5271075a66229d2104b63639
Wes John-Alder <wes@punkave.com>
2012-06-08 15:47:00 -0400

    Added a collection select widget

---
+++ test/jasmine/spec/CollectionOptionViewSpec.js
@@ -0,0 +1,30 @@
+describe("CollectionItemView", function() {
+    var testItem;
+    var itemView;
+
+    beforeEach(function() {
+        testItem = new Backbone.Model({
+            name: "TestItem"
+        });
+
+        itemView = new CollectionOptionView({
+            label:      "TestItemView",
+            model:      testItem
+        });
+        itemView.render();
+    });
+
+    it("should render when model changes", function() {
+        spyOn(itemView, "render");
+        testItem.set({ name: "NewName" });
+        expect(itemView.render).toHaveBeenCalled();
+    });
+
+    it("should be selected when the item is selected", function() {
+        console.log(itemView.$el);
+
+        expect(itemView.$el.prop('selected')).toBe(false);
+        testItem.set({ selected: true });
+        expect(itemView.$el.prop('selected')).toBe(true);
+    });
+});
