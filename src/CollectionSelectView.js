3d1f5a63184c8d20e87bc3e6f45e07d5f59d7fef
Wes John-Alder <wes@punkave.com>
2012-06-15 17:13:53 -0400

    Added addAll to collectionView initialize

--- src/CollectionSelectView.js
+++ src/CollectionSelectView.js
@@ -21,19 +21,27 @@ var CollectionSelectView = CollectionView.extend({
         return this.$el.val();
     },
 
+    addAll: function(collection, options) {
+        CollectionView.prototype.addAll.call(this, collection, options);
+        this.addEmptyOption();
+    },
+
+    addEmptyOption: function() {
+        if (this.addEmpty) {
+            var $option = $('<option />');
+            
+            this.$('option').each(function() {
+                $(this).prop('selected', false);
+            });
+
+            $option.prop('selected', true);
+            this.$el.prepend($option);
+        }
+    },
+
     render: function() {
         CollectionView.prototype.render.call(this);
-
-        if (this.addEmpty) {
-            var idAttribute = this.collection.model.idAttribute || "id";
-            var attrs = {
-                selected: true
-            };
-            attrs[idAttribute] = "";
-            this.collection.add([attrs], { at: 0 });
-        }
-
-        return this;
+        this.addEmptyOption();
     },
 
     callChangeFunction: function(event) {
