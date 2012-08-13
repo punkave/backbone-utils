f8ea47c3f32ba356e017e8054d8e9f2c1bca89e3
Wes John-Alder <wes@punkave.com>
2012-08-13 16:23:49 -0400

    Added a selected handler to CollectionOptionView

--- src/CollectionOptionView.js
+++ src/CollectionOptionView.js
@@ -1,6 +1,14 @@
 var CollectionOptionView = CollectionItemView.extend({
     tagName: 'option',
 
+    initialize: function(options) {
+        CollectionItemView.prototype.parseOptions.call(this, options);
+
+        this.model.on('change:selected', function(model, selected) {
+            this.$el.prop('selected', selected);
+        }, this);
+    },
+
     // This is a default inline template that simply lists all keys and values for the model
     getDefaultTemplate: function() {
         return _.template("<%= data.name %>");
